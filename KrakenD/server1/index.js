8c9720591719ac22b845ff065f57c1dad7977ea1
subhadip1203 <subhadip1203@gmail.com>
2022-05-29 22:31:22 +0530

    krakend api chaining

--- KrakenD/server1/index.js
+++ KrakenD/server1/index.js
@@ -10,6 +10,16 @@ app.get('/test', (req, res) => {
   res.send({success:1})
 })
 
+app.get('/chain1/:id', (req, res) => {
+  const destination_id = req.params.id
+  console.log(destination_id)
+  res.send({success:1,destination_id:destination_id})
+})
+app.get('/chain2/:destination_id', (req, res) => {
+  const destination_id = req.params.destination_id
+  console.log(destination_id)
+  res.send({success:1,destination_id:destination_id})
+})
 
 const port = 8000
 app.listen(port, () => {
