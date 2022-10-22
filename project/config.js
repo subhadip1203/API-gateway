df55f40602958c957968a64fec95d1429b6931a4
subhadip <subhadipsjsc@gmail.com>
2022-10-21 23:34:22 -0700

    project

---
+++ project/config.js
@@ -0,0 +1,14 @@
+const config = [
+	{
+		endpoint: "/v1/test" , 
+		destination: [{
+			method: 'GET',
+			url: 'https://jsonplaceholder.typicode.com/todos/1'
+		}] 
+	},
+	{endpoint: "/v1/test/:id"},
+	{endpoint: "/v1/test/ok/*"},
+	{endpoint: "/example"},
+];
+
+module.exports = config
