b6a84f3cabdf87fc792730c29326547bfe25a1bc
subhadip1203 <subhadip1203@gmail.com>
2022-10-25 22:48:42 -0700

    chain call and single call

--- project/axios/index.js
+++ project/axios/index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 async function request(method, url, options) {
   try{
     console.log({method,url,...options})
-    const ApiResult = await axios({method,url,...options})
+    const ApiResult = await axios({method,url,...options,transformResponse: x => x})
     if(ApiResult && ApiResult.data){
       const returnData = ApiResult.data;
       console.log(returnData)
